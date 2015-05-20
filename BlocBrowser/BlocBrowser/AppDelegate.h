82851656b2725eb1c2201b475c2593b01d206042
FirstName LastName <alexblair190@gmail.com>
2015-05-20 16:17:39 -0600

    checkpoint 22 complete

---
+++ BlocBrowser/BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Stephen Blair on 5/20/15.
+//  Copyright (c) 2015 blairgraphix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocBrowser/BlocBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by Stephen Blair on 5/20/15.
+//  Copyright (c) 2015 blairgraphix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
